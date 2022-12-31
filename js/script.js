902a66fd3ad2009091ee8306c416777b83912ec0
JohnAlassio <repdesystem@gmail.com>
2022-12-31 11:13:18 -0300

    Modifique el index y el script

--- js/script.js
+++ js/script.js
@@ -30,7 +30,7 @@ document.addEventListener('click', (e) =>{
 const projectsContainer = document.querySelector('.projects-container');
 const project = document.querySelector('.project');
 
-projects.forEach(project => {
+project.forEach(project => {
     const {id, title, description, list, image} = project;
     
     const projectContain = `
@@ -61,12 +61,14 @@ const modalClose = document.querySelector('.modal-close');
 function onProjectClick(id) {
     const modalContain = 
         <article class="modal" id="trabajo-${id}">
-        {/* <div class="modal-content">
+
+        <div class="modal-content">
         <a class="modal-close" href="#cerrar">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12,2C6.486,2,2,6.486,2,12s4.486,10,10,10s10-4.486,10-10S17.514,2,12,2z M16.207,14.793l-1.414,1.414L12,13.414 l-2.793,2.793l-1.414-1.414L10.586,12L7.793,9.207l1.414-1.414L12,10.586l2.793-2.793l1.414,1.414L13.414,12L16.207,14.793z"/></svg>
         </a>
         <article class="portfolio-modal">
-        <img src=${projects[id].imageModal} alt="Trabajo ${id}"> <div class="portfolio-info">
+            <img src=${projects[id].imageModal} alt="Trabajo ${id}">
+        <div class="portfolio-info">
             <h3>${projects[id].title}</h3>
             <p>${projects[id].description}</p>
             <>
@@ -83,7 +85,10 @@ function onProjectClick(id) {
             </div>
         </article>
     </div>
-    </article> */}
+    </article> 
+}
+    
+
     
     modalContainer.innerHTML = modalContain;
 }
